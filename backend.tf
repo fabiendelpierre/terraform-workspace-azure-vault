898946288c867eda91892c4cf1be78bd2ee42ce4
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2023-06-20 17:00:10 -0400

    remove variables from tf backend

--- backend.tf
+++ backend.tf
@@ -1,31 +1,9 @@
-variable "state_backend_resource_group_name" {
-  type = string
-}
-
-variable "state_backend_storage_account_name" {
-  type = string
-}
-
-variable "state_backend_storage_container_name" {
-  type = string
-}
-
-variable "state_backend_subscription_id" {
-  type = string
-}
-
-variable "state_backend_tenant_id" {
-  type = string
-}
-
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.state_backend_resource_group_name
-    storage_account_name = var.state_backend_storage_account_name
-    container_name       = var.state_backend_storage_container_name
+    resource_group_name  = "delpierref-terraform-state-storage"
+    storage_account_name = "fdtfstatestorage"
+    container_name       = "fdtfstatestorage"
     key                  = "terraform-workspace-azure-vault.tfstate"
     use_oidc             = true
-    subscription_id      = var.state_backend_subscription_id
-    tenant_id            = var.state_backend_tenant_id
   }
 }
