5c031b060346eeb34f24ed4a285356a951eda652
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2023-07-05 11:19:27 -0400

    disable resources

--- vault.tf
+++ vault.tf
@@ -127,88 +127,88 @@ resource "azurerm_network_security_rule" "main_ib_vault_https" {
   network_security_group_name                = azurerm_network_security_group.main.name
 }
 
-resource "azurerm_public_ip" "vault" {
-  name                = "${var.resource_name_prefix}-pip-vault"
-  resource_group_name = azurerm_resource_group.vault.name
-  location            = azurerm_resource_group.vault.location
-  allocation_method   = "Static"
-}
+# resource "azurerm_public_ip" "vault" {
+#   name                = "${var.resource_name_prefix}-pip-vault"
+#   resource_group_name = azurerm_resource_group.vault.name
+#   location            = azurerm_resource_group.vault.location
+#   allocation_method   = "Static"
+# }
 
-resource "azurerm_dns_a_record" "vault_public" {
-  name                = "vault"
-  zone_name           = var.dns_zone_name
-  resource_group_name = var.dns_zone_resource_group_name
-  ttl                 = 300
-  records             = [azurerm_public_ip.vault.ip_address]
-}
+# resource "azurerm_dns_a_record" "vault_public" {
+#   name                = "vault"
+#   zone_name           = var.dns_zone_name
+#   resource_group_name = var.dns_zone_resource_group_name
+#   ttl                 = 300
+#   records             = [azurerm_public_ip.vault.ip_address]
+# }
 
-resource "azurerm_network_interface" "vault" {
-  name                = "${var.resource_name_prefix}-nic-vault"
-  location            = azurerm_resource_group.vault.location
-  resource_group_name = azurerm_resource_group.vault.name
+# resource "azurerm_network_interface" "vault" {
+#   name                = "${var.resource_name_prefix}-nic-vault"
+#   location            = azurerm_resource_group.vault.location
+#   resource_group_name = azurerm_resource_group.vault.name
 
-  ip_configuration {
-    name                          = "${var.resource_name_prefix}-nic-vault"
-    subnet_id                     = azurerm_subnet.vms.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vault.id
-  }
-}
+#   ip_configuration {
+#     name                          = "${var.resource_name_prefix}-nic-vault"
+#     subnet_id                     = azurerm_subnet.vms.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.vault.id
+#   }
+# }
 
-resource "azurerm_network_interface_application_security_group_association" "vault" {
-  network_interface_id          = azurerm_network_interface.vault.id
-  application_security_group_id = azurerm_application_security_group.vault.id
-}
+# resource "azurerm_network_interface_application_security_group_association" "vault" {
+#   network_interface_id          = azurerm_network_interface.vault.id
+#   application_security_group_id = azurerm_application_security_group.vault.id
+# }
 
-resource "azurerm_linux_virtual_machine" "vault" {
-  depends_on = [
-    azurerm_key_vault_key.vault,
-    azurerm_role_assignment.msi_keyvault,
-    azurerm_role_assignment.msi_dns,
-  ]
+# resource "azurerm_linux_virtual_machine" "vault" {
+#   depends_on = [
+#     azurerm_key_vault_key.vault,
+#     azurerm_role_assignment.msi_keyvault,
+#     azurerm_role_assignment.msi_dns,
+#   ]
 
-  name                  = "${var.resource_name_prefix}-vm-vault"
-  location              = azurerm_resource_group.vault.location
-  resource_group_name   = azurerm_resource_group.vault.name
-  size                  = var.vm_size
-  admin_username        = var.vault_admin_username
-  network_interface_ids = [azurerm_network_interface.vault.id]
-  user_data = base64encode(templatefile("${path.module}/vault_vm_user_data.tpl", {
-    timezone                   = var.vm_timezone,
-    arch                       = var.vm_arch,
-    vault_version              = var.vault_version,
-    dns_zone_resource_group_id = data.azurerm_resource_group.dns_zone.id,
-    dns_zone_name              = var.dns_zone_name,
-    certbot_contact_email      = var.certbot_contact_email,
-    acme_staging               = var.acme_staging,
-    azure_tenant_id            = data.azurerm_client_config.current.tenant_id,
-    msi_id                     = azurerm_user_assigned_identity.vault.id,
-    msi_client_id              = azurerm_user_assigned_identity.vault.client_id,
-    key_vault_name             = azurerm_key_vault.vault.name,
-    key_vault_key_name         = azurerm_key_vault_key.vault.name,
-    recovery_keys              = var.vault_unseal_recovery_keys,
-    recovery_threshold         = var.vault_unseal_recovery_threshold,
-  }))
+#   name                  = "${var.resource_name_prefix}-vm-vault"
+#   location              = azurerm_resource_group.vault.location
+#   resource_group_name   = azurerm_resource_group.vault.name
+#   size                  = var.vm_size
+#   admin_username        = var.vault_admin_username
+#   network_interface_ids = [azurerm_network_interface.vault.id]
+#   user_data = base64encode(templatefile("${path.module}/vault_vm_user_data.tpl", {
+#     timezone                   = var.vm_timezone,
+#     arch                       = var.vm_arch,
+#     vault_version              = var.vault_version,
+#     dns_zone_resource_group_id = data.azurerm_resource_group.dns_zone.id,
+#     dns_zone_name              = var.dns_zone_name,
+#     certbot_contact_email      = var.certbot_contact_email,
+#     acme_staging               = var.acme_staging,
+#     azure_tenant_id            = data.azurerm_client_config.current.tenant_id,
+#     msi_id                     = azurerm_user_assigned_identity.vault.id,
+#     msi_client_id              = azurerm_user_assigned_identity.vault.client_id,
+#     key_vault_name             = azurerm_key_vault.vault.name,
+#     key_vault_key_name         = azurerm_key_vault_key.vault.name,
+#     recovery_keys              = var.vault_unseal_recovery_keys,
+#     recovery_threshold         = var.vault_unseal_recovery_threshold,
+#   }))
 
-  admin_ssh_key {
-    username   = var.vault_admin_username
-    public_key = data.azurerm_ssh_public_key.main.public_key
-  }
+#   admin_ssh_key {
+#     username   = var.vault_admin_username
+#     public_key = data.azurerm_ssh_public_key.main.public_key
+#   }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "StandardSSD_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-arm64"
-    version   = "latest"
-  }
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts-arm64"
+#     version   = "latest"
+#   }
 
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.vault.id]
-  }
-}
+#   identity {
+#     type         = "UserAssigned"
+#     identity_ids = [azurerm_user_assigned_identity.vault.id]
+#   }
+# }
